f3664011722af28270c35e91cb44791b7ce64c1b
Fernando Rodríguez-Izquierdo Serrano <fernando@rodriguezizquierdo.com>
2023-06-18 19:37:00 +0200

    [TM-005] feat: add terraform code to deploy in localhost

---
+++ terraform_local/variables.tf
@@ -0,0 +1,14 @@
+variable "db_user" {
+  type = string
+  default = "fristaskmanager"
+}
+
+variable "db_pass" {
+  type = string
+  default = "fristaskmanagerpass"
+}
+
+variable "db_root_pass" {
+  type = string
+  default = "example"
+}
---
+++ terraform_local/variables.tfvars
@@ -0,0 +1,3 @@
+db_user = "${var.DB_USER}"
+db_pass = "${var.DB_PASS}"
+db_root_pass = "${var.DB_ROOT_PASS}"
