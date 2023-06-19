962120e2e921c9d4926d2b61dca58c546ee6d82d
Fernando Rodríguez-Izquierdo Serrano <fernando@rodriguezizquierdo.com>
2023-06-19 08:11:58 +0200

    [TM-006] feat: add loggers (#5)

--- FrisTaskManagerBackend/src/main/java/com/fris/fristaskmanagerbackend/persistence/TaskDAO.java
+++ FrisTaskManagerBackend/src/main/java/com/fris/fristaskmanagerbackend/persistence/TaskDAO.java
@@ -3,6 +3,7 @@ package com.fris.fristaskmanagerbackend.persistence;
 import com.fris.fristaskmanagerbackend.api.Task;
 import io.dropwizard.hibernate.AbstractDAO;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class TaskDAO extends AbstractDAO<TaskEntity> {
 
     private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
@@ -20,49 +22,61 @@ public class TaskDAO extends AbstractDAO<TaskEntity> {
     }
 
     public Task findById(String id) {
+        log.debug("[START findById] With id {}", id);
         Transaction transaction = currentSession().beginTransaction();
         TaskEntity taskEntity;
         try {
             taskEntity = get(id);
             transaction.commit();
         } catch (Exception e) {
+            log.error("[findById] Unexpected exception ", e);
             transaction.rollback();
             throw new RuntimeException(e);
         }
-        return Objects.nonNull(taskEntity) ? entityToTask(taskEntity) : null;
+        Task task = Objects.nonNull(taskEntity) ? entityToTask(taskEntity) : null;
+        log.debug("[STOP findById] Wit task {}", task);
+        return task;
     }
 
 
     public long create(Task task) {
-        long id;
+        log.debug("[START create] With task {}", task);
+        TaskEntity taskEntity;
         Transaction transaction = currentSession().beginTransaction();
 
         try {
-            id = persist(
+            taskEntity = persist(
                     TaskEntity.builder().tittle(task.getTittle()).date(FORMATTER.parse(task.getDate())).build()
-            ).getId();
+            );
             transaction.commit();
         } catch (Exception e) {
+            log.error("[create] Unexpected exception ", e);
             transaction.rollback();
             throw new RuntimeException(e);
         }
-        return id;
+        log.debug("[STOP create] Persisted task {}", taskEntity);
+        return taskEntity.getId();
     }
 
     public List<Task> findAll() {
+        log.debug("[START findAll]");
         List<TaskEntity> taskEntities;
         Transaction transaction = currentSession().beginTransaction();
         try {
             taskEntities = list(namedTypedQuery("com.fris.fristaskmanagerbackend.persistence.TaskDAO.findAll"));
             transaction.commit();
         } catch (Exception e) {
+            log.error("[findAll] Unexpected exception ", e);
             transaction.rollback();
             throw new RuntimeException(e);
         }
-        return taskEntities.stream().map(this::entityToTask).collect(Collectors.toList());
+        List<Task> taskList= taskEntities.stream().map(this::entityToTask).collect(Collectors.toList());
+        log.debug("[STOP findAll] With taskList {}", taskList);
+        return taskList;
     }
 
     public void update(String id, Task task) {
+        log.debug("[START update] With id {} and task {}", id, task);
         Transaction transaction = currentSession().beginTransaction();
         try {
             TaskEntity taskEntity = get(id);
@@ -75,15 +89,20 @@ public class TaskDAO extends AbstractDAO<TaskEntity> {
                 throw new EntityNotFoundException();
             }
         } catch (EntityNotFoundException e) {
+            log.info("[update] Task not found with id {} and task {}", id, task);
             throw new EntityNotFoundException("Task not found with id "+id);
         } catch (Exception e) {
+            log.error("[update] Unexpected exception ", e);
             throw new RuntimeException(e);
         } finally {
+            log.info("[update] Transaction rolled back id {}", id);
             transaction.rollback();
         }
+        log.debug("[STOP update] With id {}", id);
     }
 
     public void delete(String id) {
+        log.debug("[START delete] With id {}", id);
         Transaction transaction = currentSession().beginTransaction();
         try {
             TaskEntity taskEntity = get(id);
@@ -94,15 +113,21 @@ public class TaskDAO extends AbstractDAO<TaskEntity> {
                 throw new EntityNotFoundException();
             }
         } catch (EntityNotFoundException e) {
+            log.info("[delete] Task not found with id {}", id);
             throw new EntityNotFoundException("Task not found with id "+id);
         } catch (Exception e) {
+            log.error("[delete] Unexpected exception ", e);
             throw new RuntimeException(e);
         } finally {
+            log.info("[delete] Transaction rolled back id {}", id);
             transaction.rollback();
         }
+        log.debug("[STOP delete] With id {}", id);
     }
 
     private Task entityToTask(TaskEntity taskEntity) {
+        log.debug("[]");
+        log.debug("[]");
         return Task.builder()
                 .id(taskEntity.getId())
                 .tittle(taskEntity.getTittle())
--- FrisTaskManagerBackend/src/main/java/com/fris/fristaskmanagerbackend/resources/TaskManagerResource.java
+++ FrisTaskManagerBackend/src/main/java/com/fris/fristaskmanagerbackend/resources/TaskManagerResource.java
@@ -11,11 +11,13 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.URI;
 import java.util.List;
 
+@Slf4j
 @Path("/task")
 @Produces(MediaType.APPLICATION_JSON)
 @RequiredArgsConstructor
@@ -31,14 +33,20 @@ public class TaskManagerResource {
     @Timed
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createTask(@Valid Task task) {
+        log.debug("[START createTask] With task {}", task);
         long id = taskDAO.create(task);
-        return Response.created(URI.create(StringUtils.join(PATH, id))).build();
+        URI path = URI.create(StringUtils.join(PATH, id));
+        log.debug("[STOP createTask] Task created with id {} and path {}", id, path);
+        return Response.created(path).build();
     }
 
     @GET
     @Timed
     public List<Task> getAllTasks() {
-        return taskDAO.findAll();
+        log.debug("[START getAllTasks]");
+        List<Task> taskList = taskDAO.findAll();
+        log.debug("[STOP getAllTasks] List of tasks {}", taskList);
+        return taskList;
     }
 
     @GET
@@ -47,10 +55,12 @@ public class TaskManagerResource {
     public Response getTaskById(
             @PathParam("id") @NotEmpty @Pattern(regexp = "\\d+", message = POSITIVE_NUMBER_MESSAGE) String id
     ) {
+        log.debug("[START getTaskById] With id {}", id);
         Task task = taskDAO.findById(id);
         if (task == null) {
             throwNotFoundException();
         }
+        log.debug("[STOP getTaskById] With task {}", task);
         return Response.ok().entity(task).build();
     }
 
@@ -62,11 +72,14 @@ public class TaskManagerResource {
             @PathParam("id") @NotEmpty @Pattern(regexp = "\\d+", message = POSITIVE_NUMBER_MESSAGE) String id,
             @Valid Task task
     ) {
+        log.debug("[START updateTask] With id {} and task {}", id, task);
         try {
             taskDAO.update(id, task);
         } catch (EntityNotFoundException e) {
+            log.info("[updateTask] Task not found with id {} and task {}", id, task);
             throwNotFoundException();
         }
+        log.debug("[STOP updateTask]");
         return Response.created(URI.create(StringUtils.join(PATH, id))).build();
     }
 
@@ -76,11 +89,14 @@ public class TaskManagerResource {
     public Response deleteTask(
             @PathParam("id") @NotEmpty @Pattern(regexp = "\\d+", message = POSITIVE_NUMBER_MESSAGE) String id
     ) {
+        log.debug("[START deleteTask]");
         try {
             taskDAO.delete(id);
         } catch (EntityNotFoundException e) {
+            log.info("[deleteTask] Task not found with id {}", id);
             throwNotFoundException();
         }
+        log.debug("[STOP deleteTask]");
         return Response.noContent().build();
     }
 
